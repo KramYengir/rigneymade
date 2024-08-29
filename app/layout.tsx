976341438b4b3b8e6499a8b94dd8a65df9a9f9ad
KramYengir <kramyengir@gmail.com>
2024-08-29 18:47:01 +0100

    add basic responsive navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Ubuntu } from "next/font/google";
 import "./globals.css";
 import desktopBG from "../assets/images/rigneymade-bg.webp";
 import mobileBG from "../assets/images/rigneymade-bg-mobile.webp";
+import Navbar from "@/components/Navbar";
 
 const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
@@ -23,18 +24,27 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${ubuntu.className} relative h-[100vh]`}>
-        {children}
+        <Navbar />
+        <main>{children}</main>
+
+        {/* 
+            ToDo: Implement Dark Mode
+            ToDo: Seperate BG Images into CLient Component
+            ToDo: Make dark version of BG Images
+            ToDo: Add a footer component
+        */}
+
+        <Image
+          src={desktopBG}
+          alt="faded background image of various web development graphics"
+          className="hidden lg:block absolute inset-0 -z-10 h-full w-full object-cover opacity-80"
+        ></Image>
+        <Image
+          src={mobileBG}
+          alt="faded background image of various web development graphics"
+          className="lg:hidden absolute inset-0 -z-10 h-full w-full object-cover opacity-80"
+        ></Image>
       </body>
-      <Image
-        src={desktopBG}
-        alt="faded background image of various web development graphics"
-        className="hidden lg:block absolute inset-0 -z-10 h-full w-full object-cover opacity-80"
-      ></Image>
-      <Image
-        src={mobileBG}
-        alt="faded background image of various web development graphics"
-        className="lg:hidden absolute inset-0 -z-10 h-full w-full object-cover opacity-80"
-      ></Image>
     </html>
   );
 }
