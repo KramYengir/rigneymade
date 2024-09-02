b7dc73f8c21c54efd130cb5eb672ed59a9757ce9
KramYengir <kramyengir@gmail.com>
2024-09-02 14:55:20 +0100

    add base routes

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactPage = () => {
+  return <div className="rm-container">ContactPage</div>;
+};
+
+export default ContactPage;
---
+++ app/services/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServicesPage = () => {
+  return <div className="rm-container">ServicesPage</div>;
+};
+
+export default ServicesPage;
