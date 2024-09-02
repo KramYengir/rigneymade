75ed84bb37acc4773316d2f3d30bb004f01277c3
KramYengir <kramyengir@gmail.com>
2024-09-02 15:40:28 +0100

    add custom color of blue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "rm-blue": {
+          "50": "#ecfffe",
+          "100": "#cffefe",
+          "200": "#a4fcfd",
+          "300": "#66f6fa",
+          "400": "#20e6f0",
+          "500": "#04c8d4",
+          "600": "#06a1b4",
+          "700": "#0d8191",
+          "800": "#146776",
+          "900": "#155564",
+          "950": "#073845",
+        },
+      },
     },
   },
   plugins: [],
