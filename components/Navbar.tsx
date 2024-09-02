bb1cdbbd2cc19288de8ec9e210e18d069d26064f
KramYengir <kramyengir@gmail.com>
2024-09-02 18:23:36 +0100

    add arimo font, brand & style links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,61 +1,114 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Arimo } from "next/font/google";
+
+import { usePathname } from "next/navigation";
 import ThemeToggle from "@/components/ThemeToggle";
 import blackLogo from "@/assets/images/rm-logo-black.svg";
 import blueLogo from "@/assets/images/rm-logo-blue.svg";
 
+const arimo = Arimo({
+  weight: ["600", "400", "500", "700"],
+  subsets: ["latin"],
+});
+
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname(); // Get the current path
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  // Close the menu when the window is resized to a larger screen
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 640) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const linkClasses = (path: string) =>
+    pathname === path
+      ? "text-rm-blue-700 dark:text-rm-blue-200 font-bold"
+      : "hover:text-gray-700 dark:hover:text-rm-blue-200";
+
   return (
     <nav className="bg-white/80 dark:bg-neutral-900/20 backdrop-blur-sm w-full z-10">
       <div className="rm-container">
-        <div className="flex justify-between h-16">
+        <div className="flex justify-between py-4">
           <div className="flex-1 flex justify-between">
             {/* Logo or Brand */}
-            <div className="flex-shrink-0 flex items-center">
-              <Link href="/" className="text-xl font-bold">
+            <div>
+              <Link href="/" className="flex-shrink-0 flex items-center gap-2">
                 <Image
                   src={blackLogo}
                   alt="rigneymade logo - a handwritten r m"
                   className="h-12 w-auto dark:hidden"
-                ></Image>
+                />
                 <Image
                   src={blueLogo}
                   alt="rigneymade logo - a handwritten r m"
                   className="h-12 w-auto hidden dark:block"
-                ></Image>
+                />
+                <div className="grid text-center">
+                  <p
+                    className={`${arimo.className} text-xl tracking-wide font-medium`}
+                  >
+                    RigneyMade
+                  </p>
+                  <p
+                    className={`${arimo.className} uppercase tracking-wider text-xs text-rm-blue-700 dark:text-rm-blue-300`}
+                  >
+                    Digital Designs
+                  </p>
+                </div>
               </Link>
             </div>
             {/* Desktop Menu Links */}
             <div className="hidden sm:flex sm:space-x-8 items-center">
               <Link
                 href="/"
-                className="hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+                className={`px-3 py-2 rounded-md text-sm font-medium ${linkClasses(
+                  "/"
+                )}`}
               >
                 Home
               </Link>
               <Link
                 href="/about"
-                className="hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+                className={`px-3 py-2 rounded-md text-sm font-medium ${linkClasses(
+                  "/about"
+                )}`}
               >
                 About
               </Link>
               <Link
                 href="/services"
-                className="hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+                className={`px-3 py-2 rounded-md text-sm font-medium ${linkClasses(
+                  "/services"
+                )}`}
               >
                 Services
               </Link>
               <Link
                 href="/contact"
-                className="hover:text-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+                className={`px-3 py-2 rounded-md text-sm font-medium ${linkClasses(
+                  "/contact"
+                )}`}
               >
                 Contact
               </Link>
@@ -67,7 +120,7 @@ const Navbar: React.FC = () => {
           <div className="-mr-2 flex items-center sm:hidden">
             <button
               onClick={toggleMenu}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-gray-700 focus:outline-none"
+              className="inline-flex items-center justify-center p-2 rounded-md hover:text-rm-blue-700 dark:text-rm-blue-50 dark:hover:text-rm-blue-200 focus:outline-none"
               aria-label="Toggle menu"
             >
               <svg
@@ -97,25 +150,37 @@ const Navbar: React.FC = () => {
           <div className="grid justify-start px-6 pt-2 pb-3 space-y-2">
             <Link
               href="/"
-              className="block py-2 rounded-md text-base font-medium"
+              className={`block py-2 rounded-md text-base font-medium ${linkClasses(
+                "/"
+              )}`}
+              onClick={closeMenu}
             >
               Home
             </Link>
             <Link
               href="/about"
-              className="block py-2 rounded-md text-base font-medium"
+              className={`block py-2 rounded-md text-base font-medium ${linkClasses(
+                "/about"
+              )}`}
+              onClick={closeMenu}
             >
               About
             </Link>
             <Link
               href="/services"
-              className="block py-2 rounded-md text-base font-medium"
+              className={`block py-2 rounded-md text-base font-medium ${linkClasses(
+                "/services"
+              )}`}
+              onClick={closeMenu}
             >
               Services
             </Link>
             <Link
               href="/contact"
-              className="block py-2 rounded-md text-base font-medium"
+              className={`block py-2 rounded-md text-base font-medium ${linkClasses(
+                "/contact"
+              )}`}
+              onClick={closeMenu}
             >
               Contact
             </Link>
