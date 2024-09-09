5cbcc8a8455f652a92d70bb30f083ae1c0a3269a
KramYengir <kramyengir@gmail.com>
2024-09-09 18:29:07 +0100

    add hyphens to sub-heading

--- components/Home/PortfolioSection.tsx
+++ components/Home/PortfolioSection.tsx
@@ -6,7 +6,7 @@ const PortfolioSection = () => {
       <div className="rm-container">
         <div className="grid gap-2 justify-center text-center py-24">
           <h2 className="h2">our portfolio</h2>
-          <p className="sub-heading">Some of our work</p>
+          <p className="sub-heading">- Some of our work -</p>
         </div>
       </div>
     </section>
