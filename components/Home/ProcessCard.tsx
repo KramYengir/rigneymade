34c28e6845eb9c9fe02eb5f5c3e9de63233c95be
KramYengir <kramyengir@gmail.com>
2024-09-09 16:47:46 +0100

    move components into categorised folders

---
+++ components/Home/ProcessCard.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from "react";
+import Image, { StaticImageData } from "next/image";
+
+interface ProcessCardProps {
+  icon: StaticImageData;
+  title: string;
+  description: string;
+  number: string;
+}
+
+const ProcessCard = ({
+  icon,
+  title,
+  description,
+  number,
+}: ProcessCardProps) => {
+  return (
+    <div className="relative items-stretch grid gap-6 text-center max-w-xs py-12 shadow-md rounded-2xl bg-white/80 dark:bg-neutral-900/40 backdrop-blur-sm">
+      <Image
+        src={icon}
+        alt={title}
+        className="h-32 w-auto mx-auto"
+        aria-hidden
+      />
+      <h4 className="text-xl sm:text-2xl text-rm-blue-700 dark:text-rm-blue-400 font-bold">
+        {title}
+      </h4>
+      <p className="px-16 text-balance">{description}</p>
+      <p className="absolute inset-0 top-[5%] grid items-center text-8xl font-bold text-rm-blue-600/20 dark:text-rm-blue-400/20">
+        {number}
+      </p>
+    </div>
+  );
+};
+
+export default ProcessCard;
---
+++ components/Home/ProcessSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import consultIcon from "@/assets/images/consult-icon.png";
+import designIcon from "@/assets/images/design-icon.png";
+import developIcon from "@/assets/images/develop-icon.png";
+import ProcessCard from "@/components/Home/ProcessCard";
+
+const ProcessSection = () => {
+  return (
+    <section className="bg-rm-blue-700/50 dark:bg-rm-blue-300/60 py-12">
+      <div className="rm-container flex flex-wrap justify-evenly gap-4 py-12">
+        <ProcessCard
+          icon={consultIcon}
+          title="Consult"
+          description="We start with detailed discussions to understand your vision and goals"
+          number="1"
+        />
+        <ProcessCard
+          icon={designIcon}
+          title="Design"
+          description="We create various concepts, fine-tuned via your feedback"
+          number="2"
+        />
+        <ProcessCard
+          icon={developIcon}
+          title="Develop"
+          description="Finally, we build the design into a functional, responsive, and user-friendly product"
+          number="3"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ProcessSection;
