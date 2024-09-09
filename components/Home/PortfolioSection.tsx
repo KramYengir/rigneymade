66953064edd7bb65a15ab5c50867703419a02a17
KramYengir <kramyengir@gmail.com>
2024-09-09 16:48:44 +0100

    begin profolio section

---
+++ components/Home/PortfolioSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PortfolioSection = () => {
+  return (
+    <section>
+      <div className="rm-container">
+        <div className="grid gap-2 justify-center text-center py-24">
+          <h2 className="h2">our portfolio</h2>
+          <p className="sub-heading">Some of our work</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioSection;
