68f719b5d29822c689d0499d10c59a36bf85029b
KramYengir <kramyengir@gmail.com>
2024-09-11 14:59:32 +0100

    change font to noto sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import { Ubuntu } from "next/font/google";
+import { Ubuntu, Noto_Sans } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
@@ -9,6 +9,11 @@ import BackgroundImage from "@/components/BackgroundImage";
 
 const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+});
+
+const notoSans = Noto_Sans({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${ubuntu.className} relative h-full text-neutral-700 dark:text-neutral-100 tracking-wide pb-24`}
+        className={`${notoSans.className} relative h-full text-neutral-700 dark:text-neutral-100 tracking-wide pb-24`}
       >
         <Provider>
           <Navbar />
