0d6f91d0d17152c190842ef34d7e105e24655d6f
KramYengir <kramyengir@gmail.com>
2024-10-21 18:29:59 +0100

    add payment section

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ServicesSection from "@/components/Services/ServicesSection";
+import PaymentSection from "@/components/Services/PaymentSection";
 
 const ServicesPage = () => {
   return (
-    <div>
+    <>
+      <div className="grid gap-2 justify-center text-center h1-bg">
+        <h1 className="h1">Services</h1>
+        <p className="sub-heading">- what we do -</p>
+      </div>
       <ServicesSection />
-    </div>
+      <PaymentSection />
+    </>
   );
 };
 
