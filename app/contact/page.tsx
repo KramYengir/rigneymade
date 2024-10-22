dc78d9ce27c622deb5f3ffe3df45eb9157948aa0
KramYengir <kramyengir@gmail.com>
2024-10-22 18:05:27 +0100

    add title & contact form component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,16 @@
+import ContactForm from "@/components/Contact/ContactForm";
 import React from "react";
 
 const ContactPage = () => {
-  return <div className="rm-container">ContactPage</div>;
+  return (
+    <>
+      <div className="grid gap-2 justify-center text-center h1-bg">
+        <h1 className="h1">Contact</h1>
+        <p className="sub-heading">- get in touch -</p>
+      </div>
+      <ContactForm />
+    </>
+  );
 };
 
 export default ContactPage;
