41c61a69dfc071c324e44732fb68f48aa4d5481f
KramYengir <kramyengir@gmail.com>
2024-10-22 18:05:08 +0100

    separate the h1 from the sections

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,10 @@ import ToolsSection from "@/components/About/ToolsSection";
 const AboutPage = () => {
   return (
     <div>
+      <div className="grid gap-2 justify-center text-center h1-bg">
+        <h1 className="h1">About Us</h1>
+        <p className="sub-heading">- our story -</p>
+      </div>
       <AboutUsSection />
       <ToolsSection />
     </div>
