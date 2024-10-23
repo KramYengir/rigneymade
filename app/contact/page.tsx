4421fde8388192b8979c1ecec185c705b7167b53
KramYengir <kramyengir@gmail.com>
2024-10-23 17:48:51 +0100

    begin adding text info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,7 +8,20 @@ const ContactPage = () => {
         <h1 className="h1">Contact</h1>
         <p className="sub-heading">- get in touch -</p>
       </div>
-      <ContactForm />
+      <div className="rm-container flex flex-wrap justify-center my-24">
+        <div className="max-w-screen-sm flex flex-col gap-4">
+          <h2 className="text-4xl font-extrabold">Get in touch</h2>
+          <p className="max-w-prose text-left text-balance font-light leading-6">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+            delectus saepe deleniti ullam reprehenderit autem officia qui
+            commodi incidunt eveniet quam modi officiis consequatur, explicabo
+            quo, soluta magni quisquam placeat?
+          </p>
+        </div>
+        <div className="">
+          <ContactForm />
+        </div>
+      </div>
     </>
   );
 };
