3273f6878a9de44d38c2ad51b9efbd11543665cd
KramYengir <kramyengir@gmail.com>
2024-10-24 15:49:58 +0100

    add faq preface

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import faqItems from "@/data/faqItems";
 import FAQ from "@/components/FAQ/FAQ";
 
@@ -9,6 +10,15 @@ const FAQPage = () => {
         <h1 className="h1">FAQ</h1>
         <p className="sub-heading">- Frequently Asked Questions -</p>
       </div>
+      <p className="font-light text-center text-balance mx-auto max-w-prose px-4 mt-12">
+        Find answers to common questions about our services below. If you
+        don&apos;t see what you&apos;re looking for, feel free to reach out
+        through our{" "}
+        <Link href={"/contact"} className="font-semibold">
+          Contact page
+        </Link>
+        .
+      </p>
       <FAQ
         items={faqItems}
         categories={["Web Development", "Logo Design", "General"]}
