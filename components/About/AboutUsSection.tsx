a53432d7af812ddbd7159ac94a90f893f43373ce
KramYengir <kramyengir@gmail.com>
2024-11-05 15:51:31 +0000

    begin adding content

--- components/About/AboutUsSection.tsx
+++ components/About/AboutUsSection.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const AboutUsSection = () => {
@@ -8,27 +9,37 @@ const AboutUsSection = () => {
           <img
             src={"/images/profile-img.webp"}
             alt="profile picture of Mark Rigney"
-            className=" h-80 w-auto"
+            className=" h-96 w-auto"
           />
         </div>
+
         <div className="grid gap-2">
-          <h2 className="uppercase text-2xl lg:text-4xl font-light px-2 sm:px-0">
-            Behind{" "}
-            <span className="font-bold text-rm-primary-700 dark:text-rm-primary-400">
-              RigneyMade
-            </span>
+          <h2 className="max-w-[26ch] uppercase text-2xl lg:text-4xl font-extrabold px-2 sm:px-0">
+            Crafting Digital Success for Small Businesses
           </h2>
           <p className="max-w-lg  text-balance px-2 sm:px-0">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed odit
-            quae repudiandae dolor, neque iusto numquam, voluptatibus odio
-            impedit labore illum? Fugit animi officia inventore delectus sit
-            maiores expedita. Eos?
+            I'm Mark, an Irish web developer living in Liverpool, with a genuine
+            love for helping people. After graduating in Software Development, I
+            stepped away from the office world and took a completely different
+            path, managing and cooking at a popular fish & chip shop. It was
+            during this time that I discovered how much I enjoy helping others
+            with their digital needs.
             <br />
             <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sedneque
-            iusto numquam, odit quae repudiandae dolor, neque iusto numquam,
-            voluptatibus odio impedit labore illum? Fugit animi officia
-            inventore delectus sit maiores expedita. Eos?
+            After taking on the challenge of making a website for the shop, I
+            began dedicating my free time to web development, aiming to start a
+            small business that could make digital services more accessible to
+            those who might find them daunting. Along the way, I found myself
+            also creating logos and graphics for friends and business owners — a
+            creative process that turned out to be both enjoyable and
+            fulfilling. As a result, RigneyMade expanded to include logo design,
+            bringing brands to life with both form and function.
+            <br />
+            <br />
+            At RigneyMade, I bring a commitment to approachability and
+            integrity. I&apos;m here to offer down-to-earth, honest support,
+            whether you&apos;re just starting out or ready to give your business
+            a fresh digital presence.
           </p>
         </div>
       </div>
--- components/Home/AboutSection.tsx
+++ components/Home/AboutSection.tsx
@@ -17,17 +17,19 @@ const AboutSection = () => {
           />
         </div>
         <div className="grid gap-2">
-          <p className="max-w-96 text-center md:text-left text-balance px-2 sm:px-0">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed odit
-            quae repudiandae dolor, neque iusto numquam, voluptatibus odio
-            impedit labore illum? Fugit animi officia inventore delectus sit
-            maiores expedita. Eos?
-            <br />
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed odit
-            quae repudiandae dolor, neque iusto numquam, voluptatibus odio
-            impedit labore illum? Fugit animi officia inventore delectus sit
-            maiores expedita. Eos?
+          <h3 className="uppercase text-2xl sm:text-3xl xl:text-4xl font-bold text-left">
+            <span className="font-semibold text-lg sm:text-xl xl:text-2xl">
+              Hi, I'm Mark{" "}
+            </span>
+            <br /> Your Partner in <br />
+            Web & Logo Design
+          </h3>
+          <p className="max-w-md text-center md:text-left  px-2 sm:px-0">
+            With a background in Software Development and a genuine passion for
+            helping small businesses thrive, I'm here to simplify the digital
+            side of things for those who may find it overwhelming. From creating
+            engaging websites to designing unique logos, I&apos;m committed to
+            making digital services more accessible, personal, and impactful.
           </p>
           <Link
             href={"/about"}
