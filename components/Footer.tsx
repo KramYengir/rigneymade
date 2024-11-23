65250be26743ab9fcf5a9a34a31e7f32be9c37f1
KramYengir <kramyengir@gmail.com>
2024-11-23 15:17:56 +0000

    make links functional

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Arimo } from "next/font/google";
+import Link from "next/link";
 
 const arimo = Arimo({
   weight: ["600", "400", "500", "700"],
@@ -34,19 +35,41 @@ const Footer = () => {
           <div className="grid gap-2 md:gap-4">
             <p className="uppercase font-bold">Navigation</p>
             <hr className="border-white/20 dark:border-neutral-900/20" />
-            <p className="capitalize">home</p>
-            <p className="capitalize">about us</p>
-            <p className="capitalize">services</p>
-            <p className="capitalize">portfolio</p>
-            <p className="capitalize">contact us</p>
+            <ul className="grid gap-2 md:gap-4">
+              <Link href="/" className="capitalize hover:font-semibold">
+                home
+              </Link>
+              <Link href="/about" className="capitalize hover:font-semibold">
+                about us
+              </Link>
+              <Link href="/services" className="capitalize hover:font-semibold">
+                services
+              </Link>
+              <Link href="contact" className="capitalize hover:font-semibold">
+                contact us
+              </Link>
+              <Link href="faq" className="capitalize hover:font-semibold">
+                FAQ
+              </Link>
+            </ul>
           </div>
           <div className="grid gap-2 md:gap-4">
             <p className="uppercase font-bold">Services</p>
             <hr className="border-white/20 dark:border-neutral-900/20" />
-            <p className="capitalize">Website Design</p>
-            <p className="capitalize">Website Optimization</p>
-            <p className="capitalize">Logo Design</p>
-            <p className="capitalize">Digital Solutions</p>
+            <ul className="grid gap-2 md:gap-4">
+              <li>
+                <p className="capitalize">Website Design</p>
+              </li>
+              <li>
+                <p className="capitalize">Website Optimization</p>
+              </li>
+              <li>
+                <p className="capitalize">Logo Design</p>
+              </li>
+              <li>
+                <p className="capitalize">Digital Solutions</p>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
