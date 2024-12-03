ed8c894b9218cb8073b7fe68868e3db7331876c1
KramYengir <kramyengir@gmail.com>
2024-12-03 15:04:56 +0000

    begin adding real questions

--- data/faqItems.js
+++ data/faqItems.js
@@ -2,106 +2,144 @@ const faqItems = [
   // Web Development
   {
     category: "Web Development",
-    question: "What is React?",
-    answer: "React is a JavaScript library for building user interfaces.",
+    question: "What is the process for building a website?",
+    answer:
+      "We start with a consultation to understand your needs and goals. Next, we move on to design, development, and testing before launching your website. You'll be involved every step of the way.",
   },
   {
     category: "Web Development",
-    question: "How much does it cost to build a website?",
+    question: "How long does it take to create a website?",
     answer:
-      "The cost varies depending on the complexity and features required, but most projects start at around $1,500.",
+      "The timeline varies depending on the complexity of the project, but most websites are completed within 4–6 weeks.",
   },
   {
     category: "Web Development",
-    question: "How long does it take to build a website?",
+    question: "Do I need to provide content and images for my website?",
     answer:
-      "The timeline depends on the project's complexity, but most websites generally take 4-6 weeks to complete, including design, development, and revisions.",
+      "Yes, we encourage clients to provide their own content and images to ensure the website aligns with their vision. We can also assist with content creation if needed.",
   },
   {
     category: "Web Development",
-    question: "Will my website be mobile-friendly?",
+    question: 'What does "fully responsive" mean?',
     answer:
-      "Yes, all websites we develop are responsive, meaning they are optimized for mobile, tablet, and desktop viewing.",
+      "Fully responsive means your website will automatically adjust and look great on all devices and screen sizes, including desktops, tablets, and smartphones.",
   },
   {
     category: "Web Development",
-    question: "Do you offer website maintenance?",
+    question: "Will my website show up on Google?",
     answer:
-      "Yes, we offer ongoing maintenance plans to ensure your website stays updated and secure.",
+      "Yes, we build websites with SEO best practices to ensure they are search-engine-friendly. However, getting it to the top of search results is not something we can promise as it depends on various factors and usually requires time and expertise in SEO optimization.",
   },
   {
     category: "Web Development",
-    question: "Can I update my website myself?",
+    question: "Can you update my existing website?",
     answer:
-      "Absolutely! We build websites with user-friendly content management systems (CMS) like WordPress or custom CMS solutions, so you can easily make updates.",
+      "Generally yes, but this also depends on the technology and design of your website. For example, if you're using platform-specific technologies like WordPress, we would be more comfortable creating a new website from scratch.",
+  },
+  {
+    category: "Web Development",
+    question: "What is a domain name, and do I need one?",
+    answer:
+      'A domain name is your website\'s address on the internet, for example, in www.bobsburgers.com, "bobsburgers" is the domain name. Yes, you need one for your website, and we can help you secure it.',
+  },
+  {
+    category: "Web Development",
+    question: "What platform will my website be built on?",
+    answer:
+      "Currently, we use Next.js to build our websites. This is what we are most comfortable with at the moment, but it could change in the future",
+  },
+  {
+    category: "Web Development",
+    question: "Can I make changes to my website after it's launched?",
+    answer:
+      "A CMS (see below) is included in our service, so you can make changes to your website after it's launched. But this is limited to content changes, and not to the design of the website itself.",
+  },
+  {
+    category: "Web Development",
+    question: "What is a CMS?",
+    answer:
+      "A CMS is a content management system, which is a software that allows you to manage and publish content on your website. Currently, we use Sanity.io as our CMS.",
   },
 
   // Logo Design
   {
     category: "Logo Design",
-    question: "How long does it take to create a logo?",
-    answer: "Typically, it takes about 2 weeks from concept to final design.",
+    question: "What’s included in your logo design service?",
+    answer:
+      "Our service includes consultation, initial concepts, revisions, and delivery of final designs in various formats suitable for digital and print use.",
   },
   {
     category: "Logo Design",
-    question: "What is the logo design process like?",
+    question: "How do I know what type of logo is best for my business?",
     answer:
-      "We start with a consultation to understand your brand, followed by initial sketches and concepts. After that, we refine the design based on your feedback until we reach the final version.",
+      "During our consultation, we’ll discuss your brand’s values, target audience, and industry to help create a logo that fits your business perfectly.",
   },
   {
     category: "Logo Design",
-    question: "How many revisions do I get?",
+    question: "How long does it take to design a logo?",
     answer:
-      "Our standard package includes up to 3 rounds of revisions. Additional revisions can be arranged if needed.",
+      "Typically, from concept to final design, it could be as short as a couple of weeks to a month, depending on complexity, feedback and revisions.",
+  },
+  {
+    category: "Logo Design",
+    question: "Will I own the rights to my logo?",
+    answer:
+      "Yes, once the project is complete and payment is finalized, you will have full ownership of your logo.",
+  },
+  {
+    category: "Logo Design",
+    question: "Can you rework an existing logo?",
+    answer:
+      "Yes, we can refresh or modernize your existing logo while keeping the essence of your brand intact.",
+  },
+  {
+    category: "Logo Design",
+    question: "What if I don’t know what I want in a logo?",
+    answer:
+      "That’s okay! We’ll guide you through the process by exploring styles, colors, and concepts to find what resonates with your brand.",
   },
   {
     category: "Logo Design",
     question: "What file formats will I receive?",
     answer:
-      "You will receive your logo in various formats, including PNG, JPG, and vector files like AI, EPS, and SVG for print and digital use.",
+      "You’ll get your logo in versatile formats, including PNG, JPG, and vector files (SVG) for use across all mediums.",
   },
   {
     category: "Logo Design",
-    question: "Do you offer brand guidelines with the logo?",
+    question: 'What is an "alternative design"?',
     answer:
-      "Yes, we can provide a brand guideline document that outlines logo usage, color palette, and typography for an additional fee.",
+      "An alternative design is a design which is a variation of the main design. This may mean a change to the layout or style of the design to suit a particular purpose i.e. for stationary, clothes, business cards etc.",
   },
 
   // General
   {
     category: "General",
-    question: "What are your working hours?",
-    answer: "We are available Monday to Friday from 9 AM to 5 PM.",
+    question: "How do we get started?",
+    answer:
+      "Simply contact us through the website or email us, and we’ll set up a friendly chat to discuss your needs and next steps.",
   },
   {
     category: "General",
-    question: "How can I contact you?",
+    question: "Do you require a deposit before starting work?",
     answer:
-      "You can reach us via the contact form on our website or directly through email at info@rigneymade.com.",
+      "Yes, we typically require a 50% deposit before starting, with the balance due upon project completion and satisfaction.",
   },
   {
     category: "General",
-    question: "What is your payment process?",
-    answer:
-      "We typically require a 50% deposit upfront and the remaining 50% upon project completion. Payment can be made via bank transfer or online payment methods.",
+    question: "What payment methods do you accept?",
+    answer: "We accept bank transfers and online payments via PayPal.",
   },
   {
     category: "General",
-    question: "Do you offer a money-back guarantee?",
+    question: "I’m not tech-savvy—can you guide me through the process?",
     answer:
-      "Yes, if you're not satisfied with the initial design concepts, you can request a refund before proceeding further with the project.",
-  },
-  {
-    category: "General",
-    question: "Can you help with digital marketing?",
-    answer:
-      "While our focus is on web development and design, we have partners who specialize in digital marketing and can connect you with the right resources.",
+      "Absolutely! We work closely with you to ensure everything is clear and straightforward. You don’t need to be an expert — that's what we're here for.",
   },
   {
     category: "General",
     question: "Where are you based?",
     answer:
-      "We are based in Liverpool, but we work with clients from the UK & Ireland",
+      "We are based in Liverpool, but we work with clients across the UK and Ireland.",
   },
 ];
 
